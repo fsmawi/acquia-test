df0fe90c3f56be8f057d301b3fdbeda9c9e1106b
Stephen Raghunath <straghunath@gmail.com>
2017-02-04 07:45:44 -0500

    Temporary Auto Redirect (#15)
    * TASK Temp redirect on root to auth/tokens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import {Routes, RouterModule} from '@angular/router';
 import {AuthGuard} from './core/services/auth-guard.service';
 
 const routes: Routes = [
+  // TODO: Temporary, remove when actual auth is in place, or landing page with ability go to auth is in place
+  {path: '', redirectTo: '/auth/tokens', pathMatch: 'full'},
+  // End
   {path: 'jobs', loadChildren: 'app/jobs/jobs.module#JobsModule', canActivate: [AuthGuard]},
   {path: 'auth/tokens', loadChildren: 'app/auth-tokens/auth-tokens.module#AuthTokensModule'}
 ];
