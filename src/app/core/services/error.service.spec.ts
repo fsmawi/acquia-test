3547da4f32a8c35f81d394ba5442aac102d3a6de
Stephen Raghunat <stephen.raghunat@acquia.com>
2017-02-09 13:09:39 -0800

    feat(error.service): Added Bugsnag support
    Added bugsnag support and customer error chain method to declaritively report errors to bugsnag
    
    Closes #27, MS--2242

--- src/app/core/services/error.service.spec.ts
+++ src/app/core/services/error.service.spec.ts
@@ -13,4 +13,9 @@ describe('ErrorService', () => {
   it('should ...', inject([ErrorService], (service: ErrorService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should report an event to bugsnag, and return the service for chaining', inject([ErrorService], (service: ErrorService) => {
+    const result = service.reportError(Error('123'), 'Test', {}, 'info');
+    expect(result).toEqual(jasmine.any(ErrorService));
+  }));
 });
--- src/app/core/services/error.service.ts
+++ src/app/core/services/error.service.ts
@@ -1,4 +1,7 @@
 import {Injectable} from '@angular/core';
+import {environment} from '../../../environments/environment';
+
+declare const Bugsnag;
 
 @Injectable()
 export class ErrorService {
@@ -9,5 +12,20 @@ export class ErrorService {
   apiError(e: Error) {
     // TODO
     console.error(e);
+  }
+
+  /**
+   * Reports an error to bugsnag
+   * @param e Error to report
+   * @param name Error label name
+   * @param meta Custom object to save with the error
+   * @param severity info|warning|error
+   * @returns {ErrorService}
+   */
+  reportError(e: Error, name: string, meta: any, severity: string) {
+    if (environment.production) {
+      Bugsnag.notifyException(e, name, meta, severity);
+    }
+    return this;
   }
 }
