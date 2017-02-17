784dccf511339bcb471af76ed009caa5acc7f499
SMAWI <fayssal.smawi@gmail.com>
2017-02-17 10:24:12 +0000

    MS-2337--mock-server-accomadations (#50)
    * fix(MOCK API): remove duplicate header

--- server/api/mock.php
+++ server/api/mock.php
@@ -31,6 +31,11 @@ function response($data, $status = 200)
   header("Access-Control-Allow-Orgin: *");
   header("Access-Control-Allow-Methods: *");
   header("Content-Type: application/json");
+  header("Expires: on, 01 Jan 1970 00:00:00 GMT");
+  header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+  header("Cache-Control: no-store, no-cache, must-revalidate");
+  header("Cache-Control: post-check=0, pre-check=0", false);
+  header("Pragma: no-cache");
   header("HTTP/1.1 " . $status . " " . requestStatus($status));
   echo json_encode($data);
   die();
