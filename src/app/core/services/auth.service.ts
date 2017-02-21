b8242ced9bae984a09ebe2d97323406ba2f25137
Stephen Raghunath <stephen.raghunath@acquia.com>
2017-02-21 18:44:54 -0500

    fix(auth.service): Added global headers for mocking auth
    Added global headers for mocking auth. Without, it loses the YAML header

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/map';
 import {environment} from '../../../environments/environment';
-import {Http} from '@angular/http';
+import {Http, Headers, RequestOptions} from '@angular/http';
 
 @Injectable()
 export class AuthService {
@@ -19,7 +19,19 @@ export class AuthService {
    * @returns {Promise<boolean>}
    */
   isLoggedIn(): Promise<boolean> {
-    return this.http.get(environment.apiEndpoint + '/api/v1/auth/bakery', {withCredentials: environment.production})
+    const reqOptions = new RequestOptions();
+    reqOptions.headers = reqOptions.headers || new Headers();
+
+    // All request headers
+    if (environment.headers) {
+      Object.keys(environment.headers).forEach(k => reqOptions.headers.append(k, environment.headers[k]));
+    }
+
+    // Add cookie headers
+    reqOptions.withCredentials = environment.production;
+
+    // execute
+    return this.http.get(environment.apiEndpoint + '/api/v1/auth/bakery', reqOptions)
       .map(r => r.json())
       .toPromise()
       .then(res => Promise.resolve(res.authenticated))
