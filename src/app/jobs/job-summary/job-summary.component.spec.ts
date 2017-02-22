801bc2f03b8766b59d8988dfa105309967dfe7f6
Stephen Raghunath <stephen.raghunath@acquia.com>
2017-02-22 17:26:53 -0500

    feat(job-summary.component): Added dynamic duration
    Added dynamic duration using moment for better ux.
    
    Resolves #61, MS-2434
    
    Squashed commit of the following:
    
    commit d0d67730f6606524d39c395bc55353cdd2b68b94
    Author: Stephen Raghunath <stephen.raghunath@acquia.com>
    Date:   Wed Feb 22 15:43:13 2017 -0500
    
        Debug pipelines CI Lint
    
    commit 47e2ec718cbfd0a50c3f2a996eae662ac014dd85
    Author: Stephen Raghunath <stephen.raghunath@acquia.com>
    Date:   Wed Feb 22 15:20:01 2017 -0500
    
        Fixed lint.
    
    commit 84013ac04ac0c0a052f220597a75535fec4dd809
    Author: Stephen Raghunath <stephen.raghunath@acquia.com>
    Date:   Wed Feb 22 14:53:08 2017 -0500
    
        feat(job-summary.component): Added dynamic duration
    
        Added dynamic duration using moment for better ux.

--- src/app/jobs/job-summary/job-summary.component.spec.ts
+++ src/app/jobs/job-summary/job-summary.component.spec.ts
@@ -1,12 +1,13 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {DebugElement} from '@angular/core';
 import {ElementalModule} from '../../elemental/elemental.module';
 import {JobSummaryComponent} from './job-summary.component';
 import {MomentModule} from 'angular2-moment';
 import {MaterialModule} from '@angular/material';
 import {RouterTestingModule} from '@angular/router/testing';
+import {Job} from '../../core/models/job';
 
 describe('JobSummaryComponent', () => {
   let component: JobSummaryComponent;
@@ -22,7 +23,7 @@ describe('JobSummaryComponent', () => {
         RouterTestingModule
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -33,5 +34,11 @@ describe('JobSummaryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should calculate the duration of a running job', () => {
+    component.job = new Job({requested_at: 1456263980});
+    component.calculateDuration();
+    expect(component.calculatedDuration).toMatch(/..:../);
   });
 });
--- src/app/jobs/job-summary/job-summary.component.ts
+++ src/app/jobs/job-summary/job-summary.component.ts
@@ -1,12 +1,14 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input, OnDestroy} from '@angular/core';
 import {Job} from '../../core/models/job';
+import * as moment from 'moment';
+import {Subscription, Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-job-summary',
   templateUrl: 'job-summary.component.html',
   styleUrls: ['job-summary.component.scss']
 })
-export class JobSummaryComponent implements OnInit {
+export class JobSummaryComponent implements OnInit, OnDestroy {
   /**
    * Job whose details to be shown
    */
@@ -34,14 +36,54 @@ export class JobSummaryComponent implements OnInit {
   isJobNavigationRequired = true;
 
   /**
+   * Event handler for calculating the duration
+   */
+  timer: Subscription;
+
+  /**
+   * Human friendly duration string when a job is in progress
+   * @type {string}
+   */
+  calculatedDuration = '00:00';
+
+  /**
    * Builds the component and injects services if needed
    */
-  constructor() { }
+  constructor() {
+  }
 
   /**
    * Initialize
    */
   ngOnInit() {
+    if (this.job && this.job.isUnfinished && !this.timer) {
+      // use a timer for consistent updates
+      this.timer = Observable.timer(1, 1000).subscribe(() => this.calculateDuration());
+    }
   }
 
+  /**
+   * Releases any bindings on destroy
+   */
+  ngOnDestroy() {
+    if (this.timer) {
+      this.timer.unsubscribe();
+    }
+  }
+
+  /**
+   * Calculates a human friendly duration string
+   */
+  calculateDuration() {
+    if (this.job.isUnfinished) {
+      const duration = moment.duration((+new Date()) - +moment.unix(<number>this.job.requested_at).toDate());
+      const minutes = duration.minutes();
+      const seconds = duration.seconds();
+      const normMinutes = minutes < 10 ? `0${minutes}` : minutes;
+      const normSeconds = seconds < 10 ? `0${seconds}` : seconds;
+      this.calculatedDuration = `${normMinutes}:${normSeconds}`;
+    } else {
+      this.timer.unsubscribe();
+    }
+  }
 }
