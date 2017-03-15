66c3dab8fe3af54801e44c4ae9df196710936aaf
Stephen Raghunath <straghunath@gmail.com>
2017-03-15 05:40:08 -0400

    refactor(job-detail.component): Feature flagged (#128)
    Added feature flag to disable the stream logic until backend is ready
    
    Resolved MS-2682

---
+++ src/app/core/features.ts
@@ -0,0 +1,17 @@
+/**
+ * Holds all feature flags can use static properties or getters for enabling
+ * or showing features
+ */
+import {environment} from '../../environments/environment';
+
+export const features = {
+
+  /**
+   * Log streaming flag. Waiting on MS-2590 and related tickets
+   * @returns {boolean}
+   */
+  get logStreaming() {
+    // switch to return true, or remove embedded flags for enabling
+    return environment.name === 'dev';
+  }
+};
