783d26dd372726afe429f7f025d4c064d2108036
Stephen Raghunath <stephen.raghunath@acquia.com>
2017-04-17 13:33:43 -0400

    Fixed failing e2e

--- test/e2e/features/properties.js
+++ test/e2e/features/properties.js
@@ -8,5 +8,6 @@ module.exports = {
   'header-value': 'input[name="headerValue"]',
   'app-id': '58bb63dd-57db-4d50-9a49-6b60d5921d14',
   'app-input': '[name="AppId"]',
-  'sign-in': 'button.mat-primary'
+  'sign-in': 'button.mat-primary',
+  'activity-tab': '[tabtitle="Activity"]'
 };
