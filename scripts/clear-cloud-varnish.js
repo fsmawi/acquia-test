4513224ce9c72130f82ca9da8bd6c0e1936101d5
Fayssal <fayssal.smawi@gmail.com>
2017-04-18 12:47:10 +0100

    feat(UX): use new api version

--- scripts/clear-cloud-varnish.js
+++ scripts/clear-cloud-varnish.js
@@ -1,73 +1,144 @@
-const request = require('superagent');
+const AcquiaHttpHmac = require('http-hmac-javascript');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
-// Cloud api credentials
-const CLOUD_API_ENDPOINT = process.env.PIPELINES_CLOUD_API_ENDPOINT; //'https://cloudapi.acquia.com/v1';
-const USER = process.env.N3_KEY;
-const PASSWORD = process.env.N3_SECRET;
+// Get env variables
+const APP_ID = process.env.PIPELINE_APPLICATION_ID;
+const REALM = process.env.PIPELINE_CLOUD_REALM;
+const ENV = process.env.PIPELINES_DEPLOYMENT_NAME;
+const KEY = process.env.N3_KEY;
+const SECRET = process.env.N3_SECRET;
 
-const REALM = process.argv.slice(2)[0];
-const SITE = process.argv.slice(2)[1];
-const ENV = process.argv.slice(2)[2];
+let signed_headers = {}, content_type = 'application/json';
 
-// 1 - Get All domain
-// 2 - Purge cache for each domain
-getEnvDomains()
+// Create HMAC instance.
+let hmac_config = {
+  realm: REALM,
+  public_key: KEY,
+  secret_key: SECRET
+};
+
+const HMAC = new AcquiaHttpHmac(hmac_config);
+
+var envId;
+
+getEnvironmentId()
+  .then((env) => {
+    envId = env;
+    return getDomains(envId);
+  })
   .then((domains) => {
-    return Promise.all(domains.map(domain => {
-      return purgeCache(domain.name);
+    return Promise.all(domains.map((domain) => {
+      return purgeCache(envId, domain.name);
     }));
   })
   .then((res) => {
-    res.map(item => {
-      console.log(item.description);
-    });
+    console.log(res);
   })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+  .catch((err) => console.log(err));
 
 /**
- * Get All domains for current environment
+ * Get the current environment id
  * @return {Promise}
  */
-function getEnvDomains() {
+function getEnvironmentId() {
+  let path = `https://cloud.acquia.com/api/applications/${APP_ID}/environments`;
   return new Promise((resolve, reject) => {
-    let url = CLOUD_API_ENDPOINT + `/sites/${REALM}:${SITE}/envs/${ENV}/domains.json`;
-    request
-      .get(url)
-      .auth(USER, PASSWORD)
-      .retry(3)
-      .end((err, res) => {
-        if (err) {
-          return reject(err);
+
+    xhr2promise(path, 'GET', 200)
+      .then((res) => {
+        // filter result using ENV name
+        let envs = res._embedded.items.map((item) => {
+          return {id: item.id, label: item.label, name: item.name}
+        }).filter((item) => {
+          return item.name == ENV;
+        });
+
+        // return first matching result or reject
+        if (envs.length) {
+          return resolve(envs[0].id);
         } else {
-          return resolve(res.body.filter(function (domain) {
-            return domain.name.indexOf('elb.amazonaws.com') == -1;
-          }));
+          return reject('No environment found');
         }
+      })
+      .catch((err) => {
+        return reject(err);
       });
   });
 }
 
+
 /**
- * Purge the Varnish cache for a given domain
- * @param  domain
+ * Get all domains for the given environment id
  * @return {Promise}
  */
-function purgeCache(domain) {
+function getDomains(envId) {
   return new Promise((resolve, reject) => {
-    let url = CLOUD_API_ENDPOINT + `/sites/${REALM}:${SITE}/envs/${ENV}/domains/${domain}/cache.json`;
-    request
-      .delete(url)
-      .auth(USER, PASSWORD)
-      .retry(3)
-      .end(function(err, res) {
-        if (err) {
-          return reject(err);
+
+    let path = `https://cloud.acquia.com/api/environments/${envId}/domains`;
+
+    xhr2promise(path, 'GET', 200)
+      .then((res) => {
+
+        let domains = res._embedded.items.map((item) => {
+          return {name: item.hostname}
+        });
+
+        if (domains.length) {
+          return resolve(domains);
         } else {
-          return resolve(res.body);
+          return reject('No domains found');
         }
+
+      })
+      .catch((err) => {
+        return reject(err);
       });
+  });
+}
+
+
+/**
+ * Get all domains for the given environment id
+ * @return {Promise}
+ */
+function purgeCache(envId, domain) {
+  let path = `https://cloud.acquia.com/api/environments/${envId}/domains/${domain}/actions/clear-varnish`;
+  return xhr2promise(path, 'POST', 202, true);
+}
+
+/**
+ * Make an XMLHttpRequest request and return a Promise
+ * @param  path
+ * @param  method
+ * @param  status
+ * @return {Promise}
+ */
+function xhr2promise(path, method, status) {
+  return new Promise((resolve, reject) => {
+
+    let request = new XMLHttpRequest();
+
+    request.onreadystatechange = () => {
+
+      if (request.readyState === 4) {
+
+        if (request.status !== status) {
+          return reject(request);
+        }
+
+        if (!HMAC.hasValidResponse(request)) {
+          return reject('The request does not have a valid response.');
+        }
+
+        return resolve(JSON.parse(request.responseText));
+      }
+    };
+
+    // Sign the request
+    let sign_parameters = {request, method, path, signed_headers, content_type};
+    HMAC.sign(sign_parameters);
+
+    request.setRequestHeader('Content-Type', content_type);
+    request.send();
   });
 }
