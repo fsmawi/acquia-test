ea3acbd7e834fb320520c386f96d517f34680c21
Prasannajnaeyulu <anji.it26@gmail.com>
2017-04-21 15:09:36 +0530

    test(e2etest): validate job log and scroller functionality
    Added e2e test for scroll to bottom
    
    Resolved MS-2800

---
+++ test/e2e/features/job-logs.properties.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'job-logs': '.logs',
+  'scroll-to-bottom': '#scroll-to-bottom',
+  'job-log-task-started': 'Task 1313956 has started.',
+  'log-chunks-yml-file-name': 'log-chunks.yml',
+  'job-log-step-install': 'Executing step install.',
+  'job-log-step-lint': 'Executing step lint.',
+  'job-log-step-test': 'Executing step test.',
+  'job-log-step-build': 'Executing step build.'
+};
