b679947bc1930aebb3771e1ccea070f0dca0488c
Prasannajnaeyulu <anji.it26@gmail.com>
2017-05-11 00:39:44 +0530

    MS-2995  Integration test for the api applications list (#236)
    Added integration tests for application list api
    
    Resolved MS-2995

---
+++ test/integration/pipelines-api/applications-list.spec.js
@@ -0,0 +1,57 @@
+const supertest = require('supertest');
+const expect = require('chai').expect;
+const qs = require('querystring');
+const logAPICall = require('../log-helper').logAPICall;
+
+
+describe('Pipelines API /api/v1/ci/applications/list', function () {
+  const token = process.env.N3_KEY;
+  const secret = process.env.N3_SECRET;
+  const endpoint = 'https://cloud.acquia.com';
+  const route = '/api/v1/ci/applications/list';
+  this.timeout(10000);
+
+  it('should return an array of application objects', () => {
+    return supertest(process.env.PIPELINES_API_URI)
+      .get(route)
+      .set('X-ACQUIA-PIPELINES-N3-ENDPOINT', endpoint)
+      .set('X-ACQUIA-PIPELINES-N3-KEY', token)
+      .set('X-ACQUIA-PIPELINES-N3-SECRET', secret)
+      .then((res) => {
+        try {
+          if (!res.ok && res.status !== 200) {
+            throw res.text;
+          } else {
+            expect(res.header['content-type']).to.equal('application/json');
+            expect(res.status).to.equal(200);
+            expect(res.body).to.be.a('Array');
+            expect(res.body[0]).to.be.a('Object');
+            expect(res.body[0].name).to.exist;
+            expect(res.body[0].uuid).to.exist;
+            expect(res.body[0].latest_job).toBeDefined;
+          }
+        } catch(e) {
+          logAPICall(res, route);
+          throw e;
+        }
+      });
+  });
+
+  it('should return 500 when headers are missing from request', () => {
+    return supertest(process.env.PIPELINES_API_URI)
+      .get(route)
+      .then((res) => {
+        try {
+          if (!res.ok && res.status !== 500) {
+            throw res.text;
+          } else {
+            expect(res.status).to.equal(500);
+            expect(res.text).to.contain('Failed to get list of applications from Acquia Cloud');
+          }
+        } catch(e) {
+          logAPICall(res, route);
+          throw e;
+        }
+      });
+  });
+});
