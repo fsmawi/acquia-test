b64a97c33e4e6766376ddf3fd857c2b97f909fc0
SMAWI <fayssal.smawi@gmail.com>
2017-05-17 10:09:34 +0100

    feat(UX): remove basic auth (#244)
    Opened pipelines.acquia.com
    
    Resolved MS-3016

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
-$cloudUrl = 'https://cloud.acquia.com';
-
-if (!isset($_SERVER['HTTP_REFERER']) || (isset($_SERVER['HTTP_REFERER'])
-  && strpos($_SERVER['HTTP_REFERER'], $cloudUrl) !== 0)) {
-  $username = 'Acquia';
-  $password = 'pipelines2017';
-
-  if (!(isset($_SERVER['PHP_AUTH_USER']) && ($_SERVER['PHP_AUTH_USER']==$username && $_SERVER['PHP_AUTH_PW']==$password))) {
-      header('WWW-Authenticate: Basic realm="Acquia Pipelines"');
-      header('HTTP/1.0 401 Unauthorized');
-      // Fallback message when the user presses cancel / escape
-      echo 'Access denied';
-      exit;
-  }
-}
+$file = './index.html';
 header('Content-Type: text/html');
 header('Content-Length: ' . filesize($file));
-readfile('./index.html');
+readfile($file);
