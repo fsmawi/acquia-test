66b35519d6779d612e5ad14ed3d1dd38b395ada0
SMAWI <fayssal.smawi@gmail.com>
2017-10-24 10:48:52 +0100

    test(karma.conf.js): Configure Karma to restart HeadlessChrome after disconnected
    * init
    
    * set jasmine.DEFAULT_TIMEOUT_INTERVAL
    
    * enable ode deploy and e2e tests
    
    * fix e2e failing scenario

--- test/e2e/features/standalone-application-list.properties.js
+++ test/e2e/features/standalone-application-list.properties.js
@@ -3,7 +3,7 @@ module.exports = {
   //'PIPELINES_IFRAME_URL': 'file:///server/iframeContainer.html?url=http://localhost:4200/applications/123',
   'applications-list': '#applications-list',
   'job-summary-job-id': '//a[@lifteventname="ClickJobListDetailLink"]',
-  'application-list-first-job-id': '//a[@trackeventname="ClickApplicationsListLastestJobLink"]',
+  'application-list-first-job-id': '//app-applications-list//li[1]//a[@trackeventname="ClickApplicationsListLastestJobLink"]',
   'application-list-n3-api-app': '//a[text()="N3 API"]',
   'iframe': 'iframe'
 };
