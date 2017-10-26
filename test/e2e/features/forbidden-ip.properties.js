5674bc97f8913dae59d6edb3d316b7e4e2a5ee6b
Vamsi Dharmavarapu <vamsi.akirah@gmail.com>
2017-10-26 12:57:27 +0530

    test(forbidden-ip.feature): Added E2E for forbidden-ip feature (#280)
    * SSH key for staging - pipelines ui
    
    * test(forbidden-ip.feature): Added E2E for forbidden-ip feature
    
    Added E2E for forbidden-ip feature
    
    MS-3438
    
    * style(forbidden-ip.feature): Removed extra line
    
    Removed extra line
    
    MS-3438
    
    * test(forbidden-ip.feature): update: check the code instead of the whole tittle
    
    update: check the code instead of the whole tittle
    
    MS-3438
    
    * style(forbidden-ip.feature): removed the commented step
    
    removed the commented step
    
    MS-3438
    
    * refactor(forbidden-ip.feature): Feeback Changes
    
    Feeback Changes
    
    MS-3438

---
+++ test/e2e/features/forbidden-ip.properties.js
@@ -0,0 +1,11 @@
+// Define the feature level and scenario level properties here
+module.exports = {
+  // feature scope
+  'forbidden-ip-yml-file' : 'forbidden-ip.yml',
+  'forbidden-code': '403',
+  'error-title': 'h2',
+  'homepage-link': '//a[text()=" Homepage "]',
+  'forbidden-ip-message-1': 'Oops! Looks like the application is configured to restrict access to certain IP addresses.',
+  'forbidden-ip-message-2': 'If IP whitelisting is enabled, you have to use the CLI for using Pipelines.',
+  'error-message': 'p'
+};
