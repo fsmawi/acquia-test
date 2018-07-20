3a53285a4f049268798df1c4459f10762690698e
Fayssal Smawi <fayssal.smawi@gmail.com>
2018-07-20 16:42:09 +0100

    debug

--- karma.conf.js
+++ karma.conf.js
@@ -34,6 +34,6 @@ module.exports = function (config) {
         flags: ['--headless', '--disable-gpu', '--remote-debugging-port=9222']
       }
     },
-    singleRun: false
+    singleRun: true
   });
 };
